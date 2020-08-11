ad3f8816d9c85c0596dcadd315becf45fe731890
bsucker98 <bmorashad@gmail.com>
2020-08-11 17:12:34 +0530

    new fish functions added

---
+++ .config/fish/functions/b.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function b --wraps=bat --description 'alias b=bat'
+  bat  $argv;
+end
---
+++ .config/fish/functions/np.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function np --wraps=preview_note_with_fzf --description 'alias np=preview_note_with_fzf'
+  preview_note_with_fzf  $argv;
+end
