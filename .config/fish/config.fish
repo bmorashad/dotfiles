d6658fbfa1037e7b9ed475b5135b32b1cd0a3179
bsucker98 <bmorashad@gmail.com>
2020-12-23 12:19:19 +0530

    added command to kill process (killp)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,6 +18,21 @@ end
   #end
 #end
 
+# kill process
+function kill_process --description "Kill processes"
+	set -l __kp__pid (procs | fzf --height '50%' -m | awk '{print $1}')
+	set -l __kp__kc $argv[1]
+
+	if test "x$__kp__pid" != "x"
+		if test "x$argv[1]" != "x"
+			echo $__kp__pid | xargs kill $argv[1]
+		else
+			echo $__kp__pid | xargs kill -9
+		end
+		kill_process
+	end
+end
+
 # GIT
 
 function fco -d "Fuzzy-find and checkout a branch"
---
+++ .config/fish/functions/killp.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function killp --wraps=kill_process --description 'alias killp=kill_process'
+  kill_process  $argv;
+end
