9c624135920ad632675250290d225f4d4d3fee66
Mohamed Rashad <bmorashad@gmail.com>
2024-03-05 22:35:33 +0530

    fix systemd-daemo-setup script

--- .installation-scripts/systemd-daemon-setups.sh
+++ .installation-scripts/systemd-daemon-setups.sh
@@ -5,8 +5,8 @@ sudo eopkg install vnstat
 sudo systemctl enable vnstat
 sudo systemctl start vnstat
 
-echo "Starting xremp (i.e: $GITHUB/xremap) for keyboard mapping "
-cd $GITHUB/xremap
+echo "Starting xremp (i.e: $GITHUB_APP_DIR/xremap) for keyboard mapping "
+cd $GITHUB_APP_DIR/xremap
 cp xremap.service /etc/systemd/system/xremap.service
 sudo systemctl enable xremap.service
 sudo systemctl start xremap.service
